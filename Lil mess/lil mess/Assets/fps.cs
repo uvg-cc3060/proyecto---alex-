e0be178c3f2cfab41c920d1db4848bad083c76bf
tru17189 <32879914+tru17189@users.noreply.github.com>
2019-05-29 12:44:36 -0600

    proyecto

---
+++ Lil mess/lil mess/Assets/fps.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fps : MonoBehaviour
+{
+    public Camera FPSCamera;
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    float h;
+    float v; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = horizontalSpeed * Input.GetAxis("Mouse X");
+        v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h, 0);
+        FPSCamera.transform.Rotate(v, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)){
+            transform.Translate(0, 0, 0.1f);
+        }
+        else {
+            if (Input.GetKey(KeyCode.S)) {
+                transform.Translate(0, 0, -0.1f);
+            }
+            else {
+                if (Input.GetKey(KeyCode.A)){
+                    transform.Translate(-0.1f, 0, 0);
+                }
+                else {
+                    if (Input.GetKey(KeyCode.D)){
+                        transform.Translate(0.1f, 0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
